0e6f909a7fc56948d158a136333dc8eefe83f932
Etsku520 <eetu.huovila@gmail.com>
2019-01-17 13:46:11 +0200

    1.3

--- Osa1/1.a/src/index.js
+++ Osa1/1.a/src/index.js
@@ -12,9 +12,9 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <>
-            <Part part={part1} exercises={exercises1}/>
-            <Part part={part2} exercises={exercises2}/>
-            <Part part={part3} exercises={exercises3}/>
+            <Part part={props.part1}/>
+            <Part part={props.part2}/>
+            <Part part={props.part3}/>
         </>
     )
 }
@@ -22,38 +22,44 @@ const Content = (props) => {
 const Total = (props) => {
     return (
         <p>
-            yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää
+            yhteensä {props.part1.exercises + props.part2.exercises + props.part3.exercises} tehtävää
         </p>
-        
+
     )
 }
 
 const Part = (props) => {
     return (
         <p>
-            {props.part} {props.exercises}
+            {props.part.name} {props.part.exercises}
         </p>
     )
 }
 
 const App = () => {
-  const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
+    const course = 'Half Stack -sovelluskehitys'
 
-  return (
-    <div>
-        <Header course={course}/>
-        <Content part1={part1} part2={part2} part3={part3} 
-        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-        <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      
-    </div>
-  )
+    const part1 = {
+        name: 'Reactin perusteet',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'Komponenttien tila',
+        exercises: 14
+    }
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content part1={part1} part2={part2} part3={part3}/>
+            <Total part1={part1} part2={part2} part3={part3} />
+
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
