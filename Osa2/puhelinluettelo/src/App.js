b500591910bb668a6355ccf453f41fba3c285158
Etsku520 <eetu.huovila@gmail.com>
2019-01-25 23:00:08 +0200

    2.6

---
+++ Osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  const [ newName, setNewName ] = useState('')
+
+  const onChangeHandler = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    const newPerson = {
+        name: newName
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const rows = () => persons.map(person => 
+                        <div key={person.name}>
+                            {person.name}
+                        </div>
+                    )
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <form onSubmit={addName}>
+        <div>
+          nimi: <input 
+            value={newName} 
+            onChange={onChangeHandler}
+        />
+        </div>
+        <div>
+          <button type="submit">lisää</button>
+        </div>
+      </form>
+      <h2>Numerot</h2>
+      {rows()}
+    </div>
+  )
+
+}
+
+export default App
