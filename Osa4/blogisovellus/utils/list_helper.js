b5669ea14fcf6f95d8db958ce07fafb6f417a097
Etsku520 <eetu.huovila@gmail.com>
2019-02-08 01:37:25 +0200

    4.4 ja 4.5

--- Osa4/blogisovellus/utils/list_helper.js
+++ Osa4/blogisovellus/utils/list_helper.js
@@ -2,6 +2,32 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.lenght === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  let bestBlog 
+  blogs.forEach(element => {
+    if (bestBlog === undefined)bestBlog = { 
+      title: element.title,
+      author: element.author,
+      likes: element.likes
+    }
+    if (element.likes > bestBlog.likes) bestBlog = { 
+      title: element.title,
+      author: element.author,
+      likes: element.likes
+    }
+  });
+  return bestBlog;
+}
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
