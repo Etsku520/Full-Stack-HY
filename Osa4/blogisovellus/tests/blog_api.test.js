4374f443f47ffcf8da633438093e5433126ebded
Etsku520 <eetu.huovila@gmail.com>
2019-02-08 22:29:58 +0200

    4.8 ja 4.9

---
+++ Osa4/blogisovellus/tests/blog_api.test.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.remove({})
+
+  const blogObjects = helper.biggerList
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('return right number of JSON blogs', async () => {
+  const blogs = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  
+  expect(blogs.body.length).toBe(helper.biggerList.length)
+})
+
+test('the returned blogs have id field', async () => {
+  const blogs = await api.get('/api/blogs')
+
+  console.log(blogs.body[0])
+  expect(blogs.body[0].id).toBeDefined()
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
