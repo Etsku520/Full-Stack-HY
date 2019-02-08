404fc9c9e3ca13f6613981f4913b82cc73be1acd
Etsku520 <eetu.huovila@gmail.com>
2019-02-08 23:29:44 +0200

    4.14

--- Osa4/blogisovellus/controllers/blogs.js
+++ Osa4/blogisovellus/controllers/blogs.js
@@ -15,7 +15,26 @@ blogsRouter.post('/', async (request, response) => {
     const result = await blog.save()
     response.status(201).json(result)
   } else {
-    response.status(400)
+    response.status(400).end()
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    ...body
+  }
+  try {
+    const updated = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updated.toJSON())
+  } catch (error) {
+    next(error)
   }
 })
 
--- Osa4/blogisovellus/tests/blog_api.test.js
+++ Osa4/blogisovellus/tests/blog_api.test.js
@@ -80,6 +80,14 @@ test('adding a blog without url or title returns 400 Bad request', async () => {
     .expect(400)
 })
 
+test('deleting a blog deletes a post', async () => {
+  await api.delete(`/api/blogs/${helper.biggerList[0]._id}`)
+    .expect(204)
+
+  blogs = await helper.blogsInDb()
+  expect(blogs.length).toBe(helper.biggerList.length - 1)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
