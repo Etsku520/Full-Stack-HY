829047a39821f64e84f6a320bdb91fbe9ced4a80
Etsku520 <eetu.huovila@gmail.com>
2019-02-08 22:54:05 +0200

    4.11

--- Osa4/blogisovellus/tests/blog_api.test.js
+++ Osa4/blogisovellus/tests/blog_api.test.js
@@ -47,6 +47,21 @@ test('adding a blog adds a post', async () => {
   expect(blogs.length).toBe(helper.biggerList.length + 1)
 })
 
+test('adding a blog without likes is automatically 0', async () => {
+  const newBlog = {
+    title: "Embedded in Academia",
+    author: "John Regehr",
+    url: "https://blog.regehr.org/"
+  }
+
+  await api.post('/api/blogs').send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = await helper.blogsInDb()
+  expect(blogs[blogs.length - 1].likes).toBe(0)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
