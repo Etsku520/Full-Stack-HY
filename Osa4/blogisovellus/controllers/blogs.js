4d8a7a8e0fe147ffa78acd59e7ffeab7317bb884
Etsku520 <eetu.huovila@gmail.com>
2019-02-09 22:39:21 +0200

    4.17

--- Osa4/blogisovellus/controllers/blogs.js
+++ Osa4/blogisovellus/controllers/blogs.js
@@ -1,26 +1,41 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1})
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', async (request, response) => {
-  if (!request.body.likes) {
-    request.body = { ...request.body, likes: 0}
-  }
-  const blog = new Blog({
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes
-  })
-  if (blog.url && blog.title) {
-    const result = await blog.save()
-    response.status(201).json(result)
-  } else {
-    response.status(400).end()
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    if (!request.body.likes) {
+      request.body = { ...request.body, likes: 0}
+    }
+    const users = await User.find({})
+    const user = users[0]
+    const userId = await user._id
+
+    const blog = new Blog({
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes,
+      user: userId
+    })
+
+    if (blog.url && blog.title) {
+      const result = await blog.save()
+      user.blogs = user.blogs.concat(result._id)
+      await user.save()
+      response.status(201).json(result)
+    } else {
+      response.status(400).end()
+    }
+  } catch (error) {
+    next(error)
   }
 })
 
