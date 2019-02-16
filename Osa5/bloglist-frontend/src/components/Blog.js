f55703116dd93c85270e27e45ee3da4ad005ce4a
Etsku520 <eetu.huovila@gmail.com>
2019-02-16 12:24:35 +0200

    5.6

--- Osa5/bloglist-frontend/src/components/Blog.js
+++ Osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [full, setFull] = useState(false)
+  const toggleView = () => setFull(!full)
+
+  const fullView = () => (
+    <>
+      <div onClick={toggleView}>{`${blog.title} by ${blog.author}`}</div>
+      <a href={blog.url}>{blog.url}</a>
+      <div>
+        {`${blog.likes} tykkäystä`}
+        <button onClick={() => console.log('tykätty')}>tykkää</button>
+      </div>
+      <div>lisännyt {blog.user.name}</div>
+    </>
+  )
+
+  return (
+    <div className='blog'>
+      {full ? (
+        fullView()
+      ) : (
+        <div onClick={toggleView}>
+          {blog.title} by {blog.author}
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
