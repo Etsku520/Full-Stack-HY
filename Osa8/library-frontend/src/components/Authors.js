20630d571ec618021048574946b02356a844d5e4
Etsku520 <eetu.huovila@gmail.com>
2019-03-22 22:22:51 +0200

    8.11

--- Osa8/library-frontend/src/components/Authors.js
+++ Osa8/library-frontend/src/components/Authors.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { gql } from 'apollo-boost'
-import { useQuery } from 'react-apollo-hooks'
+import { useQuery, useMutation } from 'react-apollo-hooks'
 
 const ALL_AUTHORS = gql`
 {
@@ -12,8 +12,37 @@ const ALL_AUTHORS = gql`
 }
 `
 
+const EDIT_AUTHOR = gql`
+mutation updateAuthor($name: String!, $born: Int!) {
+  editAuthor(
+    name: $name,
+    setBornTo: $born
+  ) {
+    name
+    born
+  }
+}
+`
+
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS)
+  const editAuthor = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{query: ALL_AUTHORS}]
+  })
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const updateAuhtor = async (e) => {
+    e.preventDefault()
+
+    console.log(name, born)
+    await editAuthor({
+      variables: { name, born: Number(born) }
+    })
+
+    setName('')
+    setBorn('')
+  }
 
   if (!props.show) {
     return null
@@ -24,7 +53,7 @@ const Authors = (props) => {
   }
 
   const authors = result.data.allAuthors
-  
+
   return (
     <div>
       <h2>authors</h2>
@@ -49,6 +78,26 @@ const Authors = (props) => {
         </tbody>
       </table>
 
+      <h3>Set Birthday</h3>
+      <form onSubmit={updateAuhtor}>
+        <div>
+          name:
+          <input 
+            type="text"
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          born:
+          <input
+            type="number"
+            value={born}
+            onChange={event => setBorn(event.target.value)}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
     </div>
   )
 }
